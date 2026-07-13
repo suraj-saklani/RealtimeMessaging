073d97e0cd80e6cc717fe252bdc00bcbfe91d8f5
suraj-saklani <surajsaklani.career@gmail.com>
2026-07-13 21:05:42 +0530

    adding Notification hub route

--- RealtimeMessaging.AspNetCore/ServiceCollectionExtensions.cs
+++ RealtimeMessaging.AspNetCore/ServiceCollectionExtensions.cs
@@ -1,9 +1,12 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.DependencyInjection;
 using RealtimeMessaging.Core;
 using RealtimeMessaging.Core.Interface.Repositories;
 using RealtimeMessaging.Core.Repositories;
 using RealtimeMessaging.Persistence;
 using RealtimeMessaging.SignalR.Extensions;
+using RealtimeMessaging.SignalR.Hubs;
 
 namespace RealtimeMessaging.AspNetCore
 {
@@ -31,6 +34,14 @@ namespace RealtimeMessaging.AspNetCore
             }
 
             return services;
+        }
+        public static IEndpointRouteBuilder MapRealtimeNotifications(
+            this IEndpointRouteBuilder endpoints,
+            string pattern)
+        {
+            endpoints.MapHub<NotificationHub>(pattern);
+
+            return endpoints;
         }
     }
 }
